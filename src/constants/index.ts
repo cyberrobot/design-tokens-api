57c659ad11236c9f155568f6818654d114689850
Yaroslav Salmin <yaroslav.salmin@sky.uk>
2023-07-03 11:27:55 +0100

    Example request token page protocol environment variable

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -124,4 +124,4 @@ export const sdBuildFolder = `${
 export const host =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : process.env.VERCEL_URL;
+    : `https://${process.env.VERCEL_URL as string}`;
