9df09911979b4a71769805baae3c6ad3ec6af288
Yaroslav Salmin <yaroslav.salmin@sky.uk>
2023-07-24 17:25:57 +0100

    new token page. collect token data and save in the database.

---
+++ src/components/TokenImportSource.tsx
@@ -0,0 +1,79 @@
+import { useEffect, useState } from 'react';
+import { FaGithub, FaFileImport } from "react-icons/fa6";
+import { useTokenImportStore } from '~/stores/use-token-import';
+import { api } from '~/utils/api';
+
+export default function TokenImportSource() {
+  const [gitHubPath, setGithubPath] = useState('');
+  const [activeTab, setActiveTab] = useState<number>(0)
+  const updateToken = useTokenImportStore((state) => state.updateToken);
+  const fileUploadMutation = api.import.file.useMutation();
+  const githubMutation = api.import.github.useMutation();
+  const githubImportHandler = () => {
+    if (gitHubPath === '') return;
+    githubMutation.mutate({
+      path: gitHubPath
+    });
+  };
+  const fileUploadHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target?.files?.item(0);
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsText(file, "UTF-8");
+      reader.onload = function (evt) {
+        if (evt?.target?.result) {
+          fileUploadMutation.mutate({ file: (evt?.target?.result).toString() });
+        }
+      }
+    }
+  };
+  const tabsHandler = (index: number) => {
+    setActiveTab(index)
+  }
+
+  useEffect(() => {
+    if (githubMutation.data && "content" in githubMutation.data) {
+      updateToken({
+        content: Buffer.from(githubMutation.data.content, 'base64').toString('utf-8')
+      })
+    }
+
+    if (fileUploadMutation.data) {
+      updateToken({
+        content: fileUploadMutation.data
+      })
+    }
+  }, [fileUploadMutation.data, githubMutation.data, updateToken])
+
+
+  return (
+    <>
+      <div className="flex gap-3 justify-center mb-8">
+        <button className={`${activeTab === 0 ? '' : 'btn-ghost'} btn btn-lg`} onClick={() => tabsHandler(0)}><FaGithub size={36} /></button>
+        <button className={`btn ${activeTab === 1 ? '' : 'btn-ghost'} btn-lg`} onClick={() => tabsHandler(1)}><FaFileImport size={36} /></button>
+      </div>
+      {activeTab === 0 && <div className="flex flex-col gap-8 px-4 py-8 bg-neutral rounded-md mt-5 shadow-sm">
+        <h2 className="text-2xl font-regular tracking-tight text-white">
+          GitHub import
+        </h2>
+        <div>
+          <p className="text-white">Enter the GitHub repository file path</p>
+          <div className="join mt-2 w-full">
+            <div className='join-item bg-base-100 border-base-300 border-[1px] rounded-md flex items-center px-3 pr-[1px]'>
+              <FaGithub size={24} /><span className="ml-2">https://github.com/</span>
+            </div>
+            <input value={gitHubPath} onChange={(e) => setGithubPath(e.target.value)} type="text" placeholder="path/to/the/file" className="input input-bordered join-item pl-0 focus:outline-none border-l-0 w-full" />
+            <button className="btn join-item" onClick={githubImportHandler}>Add {githubMutation.isLoading && <span className="loading loading-dots loading-sm"></span>}</button>
+          </div>
+          {githubMutation.isError && <p className="text-white">Error: {githubMutation.error.message}</p>}
+        </div>
+      </div>}
+      {activeTab === 1 && <div className="flex flex-col gap-8 px-4 py-8 bg-neutral rounded-md mt-5 shadow-sm">
+        <h2 className="text-2xl font-regular tracking-tight text-white">
+          Upload file
+        </h2>
+        <input type="file" className="file-input file-input-bordered w-full" onChange={fileUploadHandler} />
+      </div>}
+    </>
+  )
+}
--- src/server/api/routers/import-file.ts
+++ src/server/api/routers/import-file.ts
@@ -3,6 +3,7 @@ import { Octokit } from "octokit";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { prisma } from "~/server/db";
 import { type FileImport } from "@prisma/client";
+import { type ContentsRepoResponseData } from "~/types/client";
 
 export const importRouter = createTRPCRouter({
   file: publicProcedure
@@ -11,14 +12,8 @@ export const importRouter = createTRPCRouter({
         file: z.string(),
       })
     )
-    .mutation(async ({ input }): Promise<FileImport> => {
-      const fileImport = await prisma.fileImport.create({
-        data: {
-          file: input.file,
-        },
-      });
-
-      return fileImport;
+    .mutation(({ input }): string => {
+      return input.file;
     }),
   github: publicProcedure
     .input(
@@ -26,7 +21,7 @@ export const importRouter = createTRPCRouter({
         path: z.string(),
       })
     )
-    .query(async ({ input }): Promise<FileImport | null> => {
+    .mutation(async ({ input }): Promise<ContentsRepoResponseData | null> => {
       const { path } = input;
       const [repo, ...rest] = path.split("/");
       if (repo && rest) {
@@ -39,20 +34,31 @@ export const importRouter = createTRPCRouter({
             path: rest.join("/"),
           }
         );
-        if (response.data && "content" in response.data) {
-          const fileImport = await prisma.fileImport.create({
-            data: {
-              file: Buffer.from(response.data.content, "base64").toString(
-                "utf-8"
-              ),
-            },
-          });
-
-          return fileImport;
+        if (response) {
+          return response.data;
         }
 
         return null;
       }
       return null;
+    }),
+  save: publicProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        description: z.string(),
+        file: z.string(),
+      })
+    )
+    .mutation(async ({ input }): Promise<FileImport> => {
+      const fileImport = await prisma.fileImport.create({
+        data: {
+          file: input.file,
+          name: input.name,
+          description: input.description,
+        },
+      });
+
+      return fileImport;
     }),
 });
